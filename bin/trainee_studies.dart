c3bcdefa663e0a26fd571de62b941ad22add44c2
gustavojorge <gustavojorge080@gmail.com>
2025-01-25 23:58:12 -0300

    done

--- bin/trainee_studies.dart
+++ bin/trainee_studies.dart
@@ -1,14 +1,31 @@
 void main(List<String> arguments){
   String nome = "Laranja";
-  double peso = 100.2;
-  String cor = "Verde e Amarela";
-  String sabor = "Doce e cítrica";
-  int diasDesdeColheira = 40;
-  bool isMadura;
+  double peso = 98;
+  int diasDesdeColheita = 30;
+  int diasParaMadura = 20;
+  bool isMadura = funcIsMadura(diasDesdeColheita);
 
-  if (diasDesdeColheira >=30){
-    isMadura = true;
+  toString(nome: nome, peso: peso, diasDesdeColheita: diasDesdeColheita, diasParaMadura: diasParaMadura, isMadura: isMadura);
+}
+
+bool funcIsMadura(int dias){
+  if (dias >= 30){
+    return true;
   } else {
-    isMadura = false;
+    return false;
   }
+}
+
+void toString({required String nome, required double peso, required int diasDesdeColheita, required int diasParaMadura, required bool isMadura}){
+  String? madura;
+  if(funcIsMadura(diasDesdeColheita)){
+    madura = "está madura";
+  } else {
+    madura = "não está madura";
+  }
+
+  print("A $nome pesa $peso gramas! "
+        "Ela foi colhida há $diasDesdeColheita dias "
+        "e precisa de $diasParaMadura para amadurecer, "
+        "logo, a $nome $madura!");
 }
