935885974321783ce85a2a80151ef4ed719f5070
gustavojorge <gustavojorge080@gmail.com>
2025-01-26 17:17:20 -0300

    done

--- bin/trainee_studies.dart
+++ bin/trainee_studies.dart
@@ -1,32 +1,68 @@
 void main(){
-  String nome = "Laranja";
-  double peso = 98;
-  String cor = "Verde e Amarela";
+  String nome = "Banana";
+  double peso = 75;
+  String cor = "Amarela";
   String sabor = "Doce e cítrica";
   int diasDesdeColheita = 30;
   bool isMadura;
 
-  Fruta laranja = Fruta(nome, peso, cor, sabor, diasDesdeColheita);
+  Fruta macadamia = Fruta("Macadâmia", 2, "Branco Amarelado", sabor, diasDesdeColheita);
+  Fruta banana = Fruta(nome, peso, cor, sabor, diasDesdeColheita);
+  Fruta limao = Fruta("Limão", 100, "Verde", sabor, diasDesdeColheita);
+  Legume macaxeira = Legume("Macaxeira", 1200, "Marrom", true);
 
-  print(laranja.nome);
-  laranja.estaMadura(40);
+
+  macadamia.printAlimento();
+  banana.printAlimento();
+  macaxeira.printAlimento();
+  limao.printAlimento();
+  macaxeira.cozinhar();
+  limao.fazerSuco();
+
 }
 
-class Fruta{
+class Alimento{
   String nome;
   double peso;
   String cor;
+
+  Alimento(this.nome, this.peso, this.cor);
+
+  void printAlimento(){
+    print("Este(a) $nome pesa $peso gramas e é $cor");
+  }
+}
+
+class Fruta extends Alimento {
   String sabor;
   int diasDesdeColheita;
   bool? isMadura;
 
-  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});
+  Fruta(String nome, double peso, String cor, this.sabor, this.diasDesdeColheita, {this.isMadura}): super(nome, peso, cor);
 
   estaMadura(int diasParaMadura){
     isMadura = diasDesdeColheita >= diasParaMadura;
     print("A sua $nome foi colhida a $diasDesdeColheita dias, "
-          "e precisa de $diasParaMadura para poder comer. "
-          "Ela está madura? $isMadura");
+        "e precisa de $diasParaMadura para poder comer. "
+        "Ela está madura? $isMadura");
+  }
+
+  void fazerSuco(){
+    print("Você fez um ótimo suco de $nome");
+  }
+}
+
+class Legume extends Alimento{
+  bool isPrecisaCozinhar;
+
+  Legume(String nome, double peso, String cor, this.isPrecisaCozinhar) : super(nome, peso, cor);
+
+  void cozinhar(){
+    if(isPrecisaCozinhar){
+      print("Portanto, $nome está cozinhando!");
+    } else {
+      print("Nem precisa cozinhar");
+    }
   }
 }
 
